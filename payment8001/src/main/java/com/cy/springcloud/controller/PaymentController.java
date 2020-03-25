d5c37c726f53d24e8d2458b2c760602103db206b
LCY <1759416630@qq.com>
2020-03-26 00:12:54 +0800

    Eureka 集群

--- payment8001/src/main/java/com/cy/springcloud/controller/PaymentController.java
+++ payment8001/src/main/java/com/cy/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.cy.springcloud.enties.CommonResult;
 import com.cy.springcloud.enties.Payment;
 import com.cy.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****" + result);
 
